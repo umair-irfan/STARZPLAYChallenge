96bbe507e92ba43c3a2977a0241f92bb36871b88
Umair Irfan <umaironline@live.com>
2022-09-01 17:45:09 +0500

    Initial Commit + Project Setup

---
+++ StarzplayChallenge/StarzplayChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarzplayChallenge
+//
+//  Created by Umair Irfan on 01/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
